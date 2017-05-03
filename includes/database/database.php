251a102dd101288913fc40d9477ed7d5d1cdabc4
Adam <adamjhartleb@gmail.com>
2017-05-03 15:13:57 -0700

    First Commit

---
+++ includes/database/database.php
@@ -0,0 +1,30 @@
+<?php
+
+class database extends \mysqli 
+{
+  function __construct ($dbName)
+  {
+    parent::__construct('127.0.0.1', ini_get("mysqli.default_user"), ini_get("mysqli.default_pw"), $dbName);
+    if (mysqli_connect_error())
+    {
+      throw new \Exception(sprintf("Can't connect to MySQL Server. Errorcode: (%s) %s\n", mysqli_connect_errno(), mysqli_connect_error()));
+    }
+  }
+
+  function returnResults ($sql)
+  {
+    $results = $this->query($sql);
+
+    if (is_object($results))
+    {
+      if ($results->num_rows)
+      {
+        while ($row = $results->fetch_assoc())
+        {
+          $res[] = $row;
+        }
+        return $res;
+      }
+    }
+  }
+}
---
+++ includes/database/db_init.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'database.php';
+
+$db = new database('test');
+
+$sql = "CREATE TABLE admins (";
+$sql .= "id INT(11) NOT NULL AUTO_INCREMENT, ";
+$sql .= "username VARCHAR(50) NOT NULL, ";
+$sql .= "hashed_password VARCHAR(60) NOT NULL, ";
+$sql .= "PRIMARY KEY (id))";
+
+$db->query($sql);
+
+echo $sql;
